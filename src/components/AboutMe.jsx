d940117f84e4aebc06259f6fec30c3bcad90775f
alester77 <113203488+alester77@users.noreply.github.com>
2023-11-18 17:51:57 -0700

    added contact forms and image to about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import greyRockImage from '../assets/images/greyrocksmall.jpg';
 
 function AboutMe() {
   return (
     <section id="about">
-      <img src="../assets/images/greyrocksmall.jpg" alt="Selfie in the outdoors" />
+      <img className="greyRock" src={greyRockImage} alt="Selfie in the outdoors" />
       <h2>About Me</h2>
-      <p>This is where the short bio goes</p>
+      <p>I'm a graduate from Denver University and I'm looking to showcase my skills as a Full Stack Developer. I'm looking for opportunities to grow my skills and my knowledge base.</p>
     </section>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,80 @@
 import React, { useState } from 'react';
 
 function Contact() {
-  // State for form inputs and validation messages
+  // State for form inputs
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  // State for validation messages
+  const [validationMessage, setValidationMessage] = useState('');
+
+  // Handle input change
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  // Validate email format
+  const isValidEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  // Handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Check for empty fields
+    if (!formData.name || !formData.email || !formData.message) {
+      setValidationMessage('All fields are required');
+      return;
+    }
+    // Validate email
+    if (!isValidEmail(formData.email)) {
+      setValidationMessage('Please enter a valid email address');
+      return;
+    }
+
+    setValidationMessage('');
+    // Form submission logic goes here
+  };
 
   return (
     <section id="contact">
       <h2>Contact</h2>
-      <form>
-        {/* Form fields for name, email, message */}
-        {/* Validation messages */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </div>
+        {validationMessage && <p className="validationMessage">{validationMessage}</p>}
+        <button type="submit">Send Message</button>
       </form>
     </section>
   );

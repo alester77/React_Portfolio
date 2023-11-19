71d6d8c92630298272afd4b8a928058f336ef03b
alester77 <113203488+alester77@users.noreply.github.com>
2023-11-18 18:40:41 -0700

    working on project section, added place holders for now and first project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React from "react";
+import SpacePhoto from '../assets/images/projectspace.png'
 
 function Portfolio() {
-  // Assume projects is an array of project objects
-  // return (
-  //   // <section id="portfolio">
-  //   //   <h2>Portfolio</h2>
-  //   //   <div className="projects">
-  //   //     {projects.map(project => (
-  //   //       <div key={project.id} className="project">
-  //   //         <img src={project.image} alt={project.title} />
-  //   //         <h3>{project.title}</h3>
-  //   //         {/* Links to deployed app and repo */}
-  //   //       </div>
-  //   //     ))}
-  //   //   </div>
-  //   // </section>
-  // );
+  const projects = [
+    {
+      id: 1,
+      title: "Project Space",
+      imageUrl: SpacePhoto,
+      websiteUrl: "https://kalanij94.github.io/Project-Mars/",
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      imageUrl: "path_to_project_2_image.jpg",
+      websiteUrl: "https://project2.com",
+    },
+    {
+      id: 3,
+      title: "Project 3",
+      imageUrl: "path_to_project_3_image.jpg",
+      websiteUrl: "https://project3.com",
+    },
+  ];
+  return (
+    <section id="portfolio">
+      <h2>My Projects</h2>
+      <div className="project-list">
+        {projects.map((project) => (
+          <div key={project.id} className="project">
+            <a
+              href={project.websiteUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={project.imageUrl} alt={project.title} />
+              <p>{project.title}</p>
+            </a>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
